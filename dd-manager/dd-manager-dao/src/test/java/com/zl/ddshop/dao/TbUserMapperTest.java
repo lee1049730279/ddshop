7b7b71edd4726506d5836fc8c3bc7b5c027e8e6f
hasee <1049730279@qq.com>
2017-11-03 22:08:19 +0800

    工程基础环境配置（1）成功

---
+++ dd-manager/dd-manager-dao/src/test/java/com/zl/ddshop/dao/TbUserMapperTest.java
@@ -0,0 +1,24 @@
+package com.zl.ddshop.dao;
+
+import com.zl.ddshop.pojo.po.TbUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao-test.xml"})
+public class TbUserMapperTest {
+    @Autowired
+    private  TbUserMapper userdao;
+    @Test
+    public void testSelectByPrimaryKey() throws Exception {
+        //ctrl+alt+v自动补全上一部分
+        TbUser tbUser = userdao.selectByPrimaryKey(5L);
+        System.out.println(tbUser.getUsername());
+    }
+
+}
